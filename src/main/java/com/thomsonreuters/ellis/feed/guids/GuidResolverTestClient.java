2d6f9840c2d7f388dbaebcaf2088ee2549f712f2
Dmitriy Pavlov <dmitrii.pavlov2@thomsonreuters.com>
2020-08-03 12:21:08 +0300

    UKBL-3482: Guid resolver: batched tests updated

--- src/main/java/com/thomsonreuters/ellis/feed/guids/GuidResolverTestClient.java
+++ src/main/java/com/thomsonreuters/ellis/feed/guids/GuidResolverTestClient.java
@@ -1,6 +1,7 @@
 package com.thomsonreuters.ellis.feed.guids;
 
 import com.google.common.base.Stopwatch;
+import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +10,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 class GuidResolverTestClient {
@@ -22,18 +25,31 @@ class GuidResolverTestClient {
   public static void main(String[] args) throws IOException {
     final String path = "resolveBatch";
 
-    byte[] content="[\"eu/doc/legislation/binary/f74bc3e6-4ae3-11e9-a8ed-01aa75ed71a1:BUL:0\", \"eu/doc/legislation/binary/9797a8dc-efdf-4d2e-a25b-9d07a8b629bb:POR:0\",\"eu/doc/legislation/binary/6b3e4af4-9e53-11ea-9d2d-01aa75ed71a1:FRA:0\"]\n".getBytes();
+    final List<String> contexts =
+        Arrays.asList("eu/doc/legislation/binary/f74bc3e6-4ae3-11e9-a8ed-01aa75ed71a1:BUL:0",
+            "eu/doc/legislation/binary/9797a8dc-efdf-4d2e-a25b-9d07a8b629bb:POR:0",
+            "eu/doc/legislation/binary/6b3e4af4-9e53-11ea-9d2d-01aa75ed71a1:FRA:0",
+            "eu/docfamily/legislation/cellar/3b729ddf-f1f7-11e3-8cd4-01aa75ed71a1:pdfs:HUN",
+            "eu/doc/legislation/binary/e8eed569-8d69-495e-92b1-a7393e015e9c:POL:0",
+            "eu/doc/legislation/binary/a55c747d-7176-4776-9b16-6eb913087fc1:DEU:0",
+            "eu/doc/legislation/binary/b604bdc9-75db-44de-8258-b895f2adfff2:SWE:0",
+            "eu/doc/legislation/binary/f0929ffc-b99b-42cb-ac41-b415ee6e03c4:ELL:0");
+    final String content = new Gson().toJson(contexts);
     final Stopwatch started = Stopwatch.createStarted();
-    final int cnt = 1000;
+    final int cnt = 100;
     for (int i = 0; i < cnt; i++) {
-      sendPostRequest(path, content, "application/json;charset=UTF-8");
+      sendPostRequest(path, content);
 
       if (i % 4 == 0) {
-        printSpeed(started, i + 1);
+        printSpeed(started, i + 1, contexts.size());
       }
     }
 
-    printSpeed(started, cnt);
+    printSpeed(started, cnt, contexts.size());
+  }
+
+  private static void sendPostRequest(String path, String content) throws IOException {
+    sendPostRequest(path, content.getBytes(StandardCharsets.UTF_8), "application/json;charset=UTF-8");
   }
 
   public static void mainResolve(String[] args) throws IOException {
@@ -48,18 +64,20 @@ class GuidResolverTestClient {
       sendPostRequest(path, params);
 
       if (i % 4 == 0) {
-        printSpeed(started, i + 1);
+        printSpeed(started, i + 1, 1);
       }
     }
 
-    printSpeed(started, cnt);
+    printSpeed(started, cnt, 1);
   }
 
-  private static void printSpeed(Stopwatch started, int cnt) {
+  private static void printSpeed(Stopwatch started, int cnt, int size) {
     final long msElapsed = started.elapsed().toMillis();
     final double tps = cnt * 1000.0 / msElapsed;
+    final double ctxps = tps * size;
     System.out.println("N=" + cnt + ", Required " + msElapsed + " millis: " +
-        String.format("%.2f", tps) + " tps at " + HOST);
+        String.format("%.2f", tps) + " reqps, " +
+        String.format("%.2f", ctxps) + " ctx/s at " + HOST);
   }
 
   private static StringBuilder sendPostRequest(String path, Map<String, Object> params) throws IOException {
